f230fd743c7391ca11f2cf955b9948a42a69639f
kpcyrd <git@rxv.cc>
2015-06-11 23:33:29 +0200

    Set real name in torrent

--- utils.py
+++ utils.py
@@ -4,11 +4,12 @@ def make_torrent(file):
     fs = lt.file_storage()
     piece_size = 256 * 1024
     lt.add_files(fs, file.path)
+    if file.real_name:
+        fs.set_name(file.real_name)
 
     t = lt.create_torrent(fs, piece_size)
     lt.set_piece_hashes(t, file.storage_folder)
     t.add_url_seed(file.blob)
     t.set_creator('a2p')
 
-    torrent = t.generate()
-    return lt.bencode(torrent)
+    return lt.bencode(t.generate())
