6388d0bbf1a0f52d66b0cea8562bf3d6c0e48fd7
kpcyrd <git@rxv.cc>
2015-07-06 02:00:46 +0200

    Add speedbar

--- static/js/source.js
+++ static/js/source.js
@@ -17,6 +17,51 @@ window.onload = function()  {
             return cb;
         };
 
+        var SpeedBar = function(label, display) {
+            var id;
+
+            var ticks = 0;
+            var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+
+            this.update = function(size) {
+                ticks += size;
+            };
+
+            var humanize = function(bytes) {
+                var i = 0;
+
+                while(bytes > 1024 && i < units.length) {
+                    bytes /= 1024;
+                    i++;
+                }
+
+                return label + ': ' + Math.round(bytes * 100) / 100 + ' ' + units[i] + '/s';
+            };
+
+            var format = function() {
+                var x = ticks;
+                ticks = 0;
+                return humanize(x);
+            };
+
+            this.start = function() {
+                id = setInterval(function() {
+                    display(format());
+                }, 1000);
+                return this;
+            };
+
+            this.stop = function() {
+                clearInterval(id);
+            };
+
+            return this;
+        };
+
+        var updateSpeed = function(speed) {
+            document.getElementById('speed').textContent = speed;
+        };
+
         var streamFile = function(file, cb) {
             cb = wrapcb(cb, ['chunk', 'done']);
 
@@ -43,12 +88,16 @@ window.onload = function()  {
             var h = require('sha.js/sha1');
             var sha1 = new h();
 
+            var bar = new SpeedBar('hashing', updateSpeed).start();
+
             streamFile(file, {
                 chunk: function(chunk) {
                     sha1.update(new Uint8Array(chunk));
+                    bar.update(chunk.byteLength);
                 },
 
                 done: function() {
+                    bar.stop();
                     cb(sha1.digest('hex'));
                 }
             });
@@ -104,16 +153,22 @@ window.onload = function()  {
                     },
 
                     unknown: function() {
+                        var bar = new SpeedBar('uploading', updateSpeed).start();
+
                         sendFile(file.files[0], {
                             error: function(error) {
                                 alert('upload failed');
                                 window.location.reload();
                             },
                             progress: function(progress) {
+                                var last = bar.last || 0;
                                 progressBar.value = progress.loaded;
                                 progressBar.max = progress.total;
+                                bar.update(progress.loaded - last);
+                                bar.last = bar.progress;
                             },
                             done: function(xhr) {
+                                bar.stop();
                                 window.location = xhr.target.responseURL;
                             }
                         });
